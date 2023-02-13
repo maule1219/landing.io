2423a092f53863281b565c5ab0d10abb527f5ae4
maule1219 <124704332+maule1219@users.noreply.github.com>
2023-02-13 02:21:46 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
+// main.js
 document.addEventListener("DOMContentLoaded", function() {
-  var logo = document.querySelector("img");
-  logo.addEventListener("mouseover", function() {
-    logo.style.transform = "scale(1.2)";
-  });
-  logo.addEventListener("mouseout", function() {
-    logo.style.transform = "scale(1)";
+
+// Get references to the navigation menu and toggle button
+  const navigation = document.querySelector('header nav');
+  const toggleButton = document.querySelector('header button');
+
+  // Toggle the navigation menu on click of the hamburger button
+  toggleButton.addEventListener('click', function() {
+    navigation.classList.toggle('show');
+    this.classList.toggle('active');
+
+  // Get references to the form and submit button
+  const form = document.querySelector('form');
+  const submitButton = form.querySelector('button[type="submit"]');
+
+  // Show a loading spinner when the form is submitted
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    submitButton.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
+
+    // Use setTimeout to simulate an API call
+    setTimeout(function() {
+      // Remove the loading spinner and show a success message
+      submitButton.innerHTML = 'Success!';
+      form.reset();
+
+      // Show a modal with a success message
+      const modal = document.createElement('div');
+      modal.classList.add('modal');
+      modal.innerHTML = `
+        <div class="modal-content">
+          <p>You have successfully subscribed to our channel!</p>
+        </div>
+      `;
+      document.body.appendChild(modal);
+      modal.style.display = 'block';
+
+      // Close the modal after 3 seconds
+      setTimeout(function() {
+        modal.style.display = 'none';
+        document.body.removeChild(modal);
+      }, 3000);
+    }, 1000);
   });
 });

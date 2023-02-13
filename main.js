378bb528c37419eb3e21d2119ec90239cbeabd21
maule1219 <124704332+maule1219@users.noreply.github.com>
2023-02-13 01:51:21 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-// mouse.js
+// main.js
 document.addEventListener("DOMContentLoaded", function() {
   var box = document.querySelector(".box");
-  box.addEventListener("mouseover", function() {
-    box.style.backgroundColor = "red";
+  box.addEventListener("mouseenter", function() {
+    box.style.backgroundColor = "#333";
+    box.style.color = "#fff";
+    box.innerHTML = "Subscribe for updates";
   });
-  box.addEventListener("mouseout", function() {
-    box.style.backgroundColor = "yellow";
+  box.addEventListener("mouseleave", function() {
+    box.style.backgroundColor = "#ffd700";
+    box.style.color = "#333";
+    box.innerHTML = "";
   });
 });
+

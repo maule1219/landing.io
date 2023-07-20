b29179ac00abae41d445cbb9a64d5d51cc0111d0
maule1219 <124704332+maule1219@users.noreply.github.com>
2023-07-20 14:43:51 +0100

    Update modals.js

--- modals.js
+++ modals.js
@@ -2,4 +2,26 @@
 document.addEventListener("DOMContentLoaded", function() {
   var trigger = document.querySelector("input[type='submit']");
   var modal = document.createElement("div");
-  modal
+  modal.classList.add("modal"); // Adding a class to style the modal
+
+  // Assuming there's a modal content to display
+  var modalContent = document.createElement("div");
+  modalContent.textContent = "This is the modal content.";
+  modal.appendChild(modalContent);
+
+  // Function to show the modal
+  function showModal() {
+    modal.style.display = "block";
+  }
+
+  // Function to hide the modal
+  function hideModal() {
+    modal.style.display = "none";
+  }
+
+  // Attach the click event to the trigger button
+  trigger.addEventListener("click", showModal);
+
+  // Append the modal to the document body
+  document.body.appendChild(modal);
+});
